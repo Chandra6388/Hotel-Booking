e662ad921bdc20d744b237c5e5fd8aa4dd847a48
Chandra6388 <cppatel6388@gmail.com>
2025-06-27 23:17:19 +0530

    doen

--- Frontend/src/pages/Index.tsx
+++ Frontend/src/pages/Index.tsx
@@ -113,14 +113,7 @@ export default function Index() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      
-
-
-
-
-
-
-      
+   
       <main className="flex-1">
         <HeroSection />
         <section id="welcome" className="section">
